f7abf9b6a66568ca87a9202d0ca5c05ff3a69714
rwarou <rwarou.yim@gmail.com>
2021-09-07 17:53:49 +0900

    create ejs \n create gitignore

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'rwarou`s blog index page' });
+  res.render('index', { title: 'rwarou`s blog', message: 'rwarou`s blog index page' });
 });
 
 module.exports = router;
